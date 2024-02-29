9c31dd8dee7fb1a0aa6c6c2b2a47e2fe50748450
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2024-02-29 12:22:39 +0530

    step 5

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+ ], (Controller) => {
+    "use strict";
+ 
+    return Controller.extend("ui5.walkthrough.controller.App", {
+       onShowHello() {
+          // show a native JavaScript alert
+          alert("Hello World");
+       },
+       onShowHi() {
+          // show a native JavaScript alert
+          alert("Hi");
+       }
+    });
+ });
